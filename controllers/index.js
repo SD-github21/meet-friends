fddcca518999dce66d9ee9f67113ff7e5def8363
Azarill <heierms@gmail.com>
2022-06-23 13:23:04 -0500

    added home routes

---
+++ controllers/index.js
@@ -0,0 +1,19 @@
+// Init router
+const router = require('express').Router();
+// Link to /api related routes
+const apiRoutes = require('./api');
+// Link to homepage related routes 
+const homeRoutes = require('./home-routes');
+// Link to dashboard related routes
+const dashboardRoutes = require('./dashboard-routes.js');
+
+//router.use('/dashboard', dashboardRoutes);
+//router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
+
+router.use((req,res) =>{
+    res.status(404).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(session(sess));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
