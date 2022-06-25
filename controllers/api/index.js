f4b6a3811baf7441d9b7f35486691596eb0bb573
SD-github21 <sapandonde.gmail.com>
2022-06-25 17:52:39 -0500

    added unique activity routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const activityRoutes = require('./activity-routes.js');
+const uniqueActivityRoutes = require('./unique-activity-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/activities', activityRoutes);
+router.use('/unique', uniqueActivityRoutes);
+
 
 module.exports = router;
---
+++ controllers/api/unique-activity-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { User, Activity, UserActivity, UniqueActivity } = require('../../models');
+
+router.get('/', (req, res) => {
+    UniqueActivity.findAll()
+     .then(dbUniqueData => res.json(dbUniqueData))
+     .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+     });
+
+});
+
+router.post('/', (req, res) => {
+    UniqueActivity.create({
+        uactivity_location: req.body.uactivity_location,
+        uactivity_address: req.body.uactivity_address,
+        user_id: req.body.user_id,
+        activity_id: req.body.activity_id
+    })
+     .then(dbUniqueData => res.json(dbUniqueData))
+     .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+     });
+
+});
+
+router.delete('/:id', (req,res) => {
+    
+
+});
+
+module.exports = router;
