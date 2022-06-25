b970fba6322d3ddf2e7692bb7753fcd7dd662492
Azarill <heierms@gmail.com>
2022-06-25 16:06:14 -0500

    updated routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,6 +12,26 @@ router.get('/', (req, res) => {
       });
 });
 
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+   .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+   })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
+
 
 router.post('/', (req,res) => {
   /*
@@ -45,6 +65,68 @@ router.post('/', (req,res) => {
         res.status(500).json(err);
       });
 
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+   .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+   });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+   .then(dbUserData => {
+    if (!dbUserData[0]) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    } 
+    res.json(dbUserData);
+   })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) =>{
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+   .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+   })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/signup', (req,res)=>{
