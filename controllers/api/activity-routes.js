a8eb76b43832a030c84e9e83443dde4cf6742d6e
SD-github21 <sapandonde.gmail.com>
2022-06-25 17:17:17 -0500

    commented out activity routes that might not be usable

--- controllers/api/activity-routes.js
+++ controllers/api/activity-routes.js
@@ -50,61 +50,61 @@ router.get('/:id', (req, res) => {
      });
 });
 
-// create a activity
-router.post('/', (req, res) => {
-    Activity.create({
-        activity_name: req.body.activity_name,
-        user_id: req.body.user_id
-        })
-     .then(dbActivityData => res.json(dbActivityData))
-     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-     });
-});
+// // create a activity
+// router.post('/', (req, res) => {
+//     Activity.create({
+//         activity_name: req.body.activity_name,
+//         user_id: req.body.user_id
+//         })
+//      .then(dbActivityData => res.json(dbActivityData))
+//      .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//      });
+// });
 
-// update an activity's name
-router.put('/:id', (req, res) => {
-    Activity.update(
-        {
-            activity_name: req.body.activity_name
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-     .then(dbActivityData => {
-        if (!dbActivityData) {
-            res.status(404).json({ message: 'No activity found with this id' });
-            return;
-        }
-        res.json(dbActivityData);
-     })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-});
+// // update an activity's name
+// router.put('/:id', (req, res) => {
+//     Activity.update(
+//         {
+//             activity_name: req.body.activity_name
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         }
+//     )
+//      .then(dbActivityData => {
+//         if (!dbActivityData) {
+//             res.status(404).json({ message: 'No activity found with this id' });
+//             return;
+//         }
+//         res.json(dbActivityData);
+//      })
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+// });
 
-router.delete('/:id', (req, res) => {
-    Activity.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-     .then(dbActivityData => {
-        if (!dbActivityData) {
-            res.status(404).json({ message: 'No activity found with this id' });
-            return;
-        }
-        res.json(dbActivityData);
-     })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      })
-})
+// router.delete('/:id', (req, res) => {
+//     Activity.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//      .then(dbActivityData => {
+//         if (!dbActivityData) {
+//             res.status(404).json({ message: 'No activity found with this id' });
+//             return;
+//         }
+//         res.json(dbActivityData);
+//      })
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       })
+// })
 
 module.exports = router;
