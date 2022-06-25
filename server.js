7b9d1e874448e8fe2110c02c2a9527be56ec70aa
SD-github21 <sapandonde.gmail.com>
2022-06-25 17:03:35 -0500

    successfully added seed data

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // turn on routes
 app.use(routes);
