6a746144c9ec4556768289db14995e2e77dfabe8
Azarill <heierms@gmail.com>
2022-06-28 19:20:32 -0500

    fixed issue with logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -272,6 +272,17 @@ router.post('/upload', upload.single('image'), (req, res) => {
     });
  });
 
+ router.post('/logout',(req,res) =>{
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+
+})
 
 
 module.exports = router;
